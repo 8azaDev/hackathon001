2baaf94d38160fe4db0a5d34cf129981fb870254
RiberLi <871353782@qq.com>
2016-08-31 23:10:58 +0800

    迷宫写法完善

--- 3组-走迷宫-riber-jeffrey/MoveMaze/MoveMaze/Program.cs
+++ 3组-走迷宫-riber-jeffrey/MoveMaze/MoveMaze/Program.cs
@@ -1,71 +1,105 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace MoveMaze
 {
     class Program
     {
-        static HashSet<string> _Visited = new HashSet<string>();
-
         static void Main(string[] args)
         {
-            //char[,] map = new char[8, 8]
-            //{
-            //    { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //    { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //     { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //      { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //       { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //          { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //      { '.', '.' , '.', '.' , '.', '.' , '.', '.'},
-            //     { '.', '.' , '.', '.' , '.', '.' , '.', '.'}
-            //};
-
-            char[,] map = new char[3, 3]
-{
-                            { '.', '.' , '.'},
-                            { '.', '.', '.' },
-                                            { '.', '.', '.' }
-
+            char[,] map = new char[10, 8]
+            {
+              {'.','#','#','#','#','#','#','#'},
+              {'.','.','.','.','.','.','.','.'},
+              {'#','#','#','#','#','#','#','.'},
+              {'.','.','.','.','.','.','.','.'},
+              {'.','#','#','#','#','#','#','#'},
+              {'.','.','.','.','.','.','.','.'},
+              {'#','#','#','#','#','#','#','.'},
+              {'.','.','.','.','.','.','.','.'},
+              {'.','#','#','#','#','#','#','.'},
+              {'#','#','#','#','#','#','#','.'}
             };
-            Console.WriteLine(MoveTo(2, 2, map));
+            var maxX = map.GetLength(0);
+            var maxY = map.GetLength(1);
+            Console.WriteLine(MoveTo(new Point(maxX-1, maxY-1), map));
         }
 
-        static int MoveTo(int m, int n, char[,] map)
+        static int MoveTo(Point point, char[,] map)
         {
             var maxX = map.GetLength(0);
             var maxY = map.GetLength(1);
-
-            if (m < 0 || m >= maxX || n < 0 || n >= maxY || map[m, n] == '#')
+            var m = point.X;
+            var n = point.Y;
+            if (m < 0 || m >= maxX || n < 0 || n >= maxY || map[m, n] == '#' || point.HasViewed())
                 return 100;
-
-            var key = $"{m},{n}";
-            Console.WriteLine(key);
-            if (_Visited.Contains(key))
-                return 100;
-
-            _Visited.Add(key);
 
             if (m == 0 && n == 0)
             {
+                Console.WriteLine($"路径为：{string.Join(",", point.ViewedKeys)},(0,0)");
                 return 0;
             }
+            var point1 = new Point(m + 1, n, point);
+            int minStep1 = m + 1 >= maxX ? 100 : MoveTo(point1, map);
 
+            var point2 = new Point(m, n + 1, point);
+            int minStep2 = n + 1 >= maxY ? 100 : MoveTo(point2, map);
 
-            int minStep1 = m + 1 > maxX ? 100 : MoveTo(m + 1, n, map);
-            int minStep2 = n + 1 > maxY ? 100 : MoveTo(m, n + 1, map);
-            int minStep3 = n - 1 < 0 ? 100 : MoveTo(m, n - 1, map);
-            int minStep4 = m - 1 < 0 ? 100 : MoveTo(m - 1, n, map);
-            Console.WriteLine($"{minStep1},{minStep2},{minStep3},{minStep4}");
-            int minStep = Math.Min(Math.Min(minStep1, minStep2), Math.Min(minStep3, minStep4)) + 1;
+            var point3 = new Point(m, n - 1, point);
+            int minStep3 = n - 1 < 0 ? 100 : MoveTo(point3, map);
 
-
-            //if (minStep > (m + 1) * (n + 1))in
-            //    return 100;
-            return minStep;
+            var point4 = new Point(m - 1, n, point);
+            int minStep4 = m - 1 < 0 ? 100 : MoveTo(point4, map);
+            int minStep = minStep1;
+            if (minStep > minStep2)
+            {
+                minStep = minStep2;
+            }
+            if (minStep > minStep3)
+            {
+                minStep = minStep3;
+            }
+            if (minStep > minStep4)
+            {
+                minStep = minStep4;
+            }
+            return minStep + 1;
         }
     }
 
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point(int x, int y, Point prePoint)
+        {
+            X = x;
+            Y = y;
+            ViewedKeys.AddRange(prePoint.ViewedKeys);
+            ViewedKeys.Add(prePoint.GetKey());
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public List<string> ViewedKeys { get; set; } = new List<string>();
+
+        public string GetKey()
+        {
+            return $"({X},{Y})";
+        }
+
+        public bool HasViewed()
+        {
+            return ViewedKeys.Contains(GetKey());
+        }
+    }
 
 }
-
