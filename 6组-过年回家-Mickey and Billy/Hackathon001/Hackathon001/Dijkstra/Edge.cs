2852e6d6d391c6840251f65d0185dbaf2d09ecd3
Mickey Hu <mickey.hu@hirede.com>
2016-08-31 20:32:52 +0800

    实现最短路径算法。

---
+++ 6组-过年回家-Mickey and Billy/Hackathon001/Hackathon001/Dijkstra/Edge.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hackathon001.Dijkstra
+{
+    public class Edge
+    {
+        public string StartNodeID;
+        public string EndNodeID;
+        public double Weight; //权值，代价        
+    }
+}
